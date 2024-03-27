46e2ee93c84adb0d9c25a878e958239b83a59d0c
VietBac <81665443+vietbac0967@users.noreply.github.com>
2024-03-27 20:55:29 +0700

    finish call api cau 2a

--- RegisterService/src/main/java/com/bac/se/registerservice/controllers/UserController.java
+++ RegisterService/src/main/java/com/bac/se/registerservice/controllers/UserController.java
@@ -20,4 +20,11 @@ public class UserController {
     public User getUserById(@PathVariable("id") Long id) {
         return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Not Found User"));
     }
+
+    @GetMapping
+    public User getUserByUsernameAndPassword(@RequestParam("username") String username,
+                                             @RequestParam("password") String password) {
+        return userRepository.findByUsernameAndPassword(username, password)
+                .orElseThrow(() -> new RuntimeException("Not found user"));
+    }
 }
--- RegisterService/src/main/java/com/bac/se/registerservice/repositories/UserRepository.java
+++ RegisterService/src/main/java/com/bac/se/registerservice/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.bac.se.registerservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsernameAndPassword(String username,String password);
 }
